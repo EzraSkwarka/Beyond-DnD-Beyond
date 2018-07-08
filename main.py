a3768117ff83b64c0876eadf120e434265688e37
Ezra Skwarka <skwarkae@berea.edu>
2018-07-07 23:11:11 -0400

    Converted Races from if/else to a dictionary Built template for racial abilities Added comments and function descriptions Added Name functionality Cried Did some pep8 and polish

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # Header Block
 ##############
 import raceClasses
+import raceTraits
 
-class Toon():
+class Toon:
     def __init__(self):
-        ##### Race info
+        # ### Biography info
+        self.playerName = ""
+        self.characterName = ""
+        # ### Race info
         self.raceTag = ""
         self.race = ""
         self.strMod = 0
@@ -15,7 +19,8 @@ class Toon():
         self.wisMod = 0
         self.chrMod = 0
         self.statMods = [self.strMod, self.dexMod, self.conMod, self.intMod, self.wisMod, self.chrMod]
-        ##### Stat Block
+        self.raceTraits = None
+        # ### Stat Block
         self.strength = 0
         self.dexterity = 0
         self.constitution = 0
@@ -23,35 +28,75 @@ class Toon():
         self.wisdom = 0
         self.charisma = 0
         self.stats = [self.strength, self.dexterity, self.constitution, self.intelligence, self.wisdom, self.charisma]
-        self.statList=["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+        self.statList = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+        # ### Class Info
 
-    def importRaceStatMods(self):
-        self.race = raceClasses.race_stat_blocks()
+    def set_names(self):
+        # Player Name
+        self.playerName = input("What is your name?\n")
+        while True:
+            print("Ah, so your name is {0}.".format(self.playerName))
+            temp = input("Is that correct? (Yes/No)\n").lower()
+            if temp == "yes":
+                break
+            else:
+                self.playerName = input("I'm very sorry I misunderstood. So then, what is your name?\n")
+        # Character Name
+        print("awesome, now that I know your name {0}, I need to know what your character is named.".format(self.playerName))
+        self.characterName = input("What is your character's name?")
+        while True:
+            print("Ah, so their name is {0}.".format(self.characterName))
+            temp = input("is that correct? (Yes/No)").lower()
+            if temp == "yes":
+                break
+            else:
+                self.characterName = input("I'm very sorry I misunderstood. So then, what is your character's name?")
+
+    def import_race_stat_mods(self):
+        """
+        Pulls the stat modifiers from the raceClasses.py file and writes them into the toon class under stat mods.
+        :return: None
+        """
+        self.race = raceClasses.set_race()
         self.raceTag = self.race.nameOfRace
         for i in range(6):
             self.statMods[i] = self.race.statMods[i]
-            print(self.statMods[i])
+        self.raceTraits = self.race.racialAbilities
+        # for i, val in range(len(self.raceTraits)):
+        #     print(self.raceTraits)
 
-
-    def statBlock(self):
-        for i in range(6):
-            self.stats[i] = int(input("set {0} to: ".format(self.statList[i])))
-        print("""Your stats are: {0}, {1}, {2}, {3}, {4}, {5}""".format(*self.stats))
-        ### add race mods
-        ##### import stat mods
+    def apply_race_stat_mods(self):
+        """
+        Pulls the racial mod info from the toon race stat block and adds the modifiers to the toon stat block. Still needs polish
+        :return: None
+        """
         print("adding race mods")
         for i in range(6):
             self.stats[i] += self.statMods[i]
         print("""Your stats are: {0}, {1}, {2}, {3}, {4}, {5}""".format(*self.stats))
 
+    def set_toon_stat_block(self):
+        """
+        When called opens the console to set the stat block using input. As of now will error out if the user does not input an int. Does not apply racial mods. Still needs polish.
+        :return:
+        """
+        for i in range(6):  # TODO: Set input to not error out on non-int input
+            self.stats[i] = int(input("set {0} to: ".format(self.statList[i])))
+        print("""Your stats are: {0}, {1}, {2}, {3}, {4}, {5}""".format(*self.stats))
+
+    # def get_race_traits(self):
+    #     self.race = raceClasses
+    #     self.raceTraits = self.race.racialAbilities()
+
 
 def main():
     playerChar = Toon()
-
-    # raceClasses.testImport()
-    playerChar.importRaceStatMods()
-    playerChar.statBlock()
-
+    # print("at some point, this will be an introductory schpeel.")
+    # playerChar.set_names()
+    playerChar.import_race_stat_mods()
+    playerChar.set_toon_stat_block()
+    playerChar.apply_race_stat_mods()
+    # playerChar.get_race_traits()
 
 
 main()
--- raceClasses.py
+++ raceClasses.py
@@ -1,12 +1,12 @@
 ##############
 # Header Block
-# Keep stats for races and refrence race traits, info for the traits in race traits page
+# Keep stats for races and reference race traits, info for the traits in race traits page
 ##############
 import raceTraits
 
 
 class RaceTemplate:
-    def __init__(self, race_tag, str_mod, dex_mod, con_mod, int_mod, wis_mod, chr_mod, racial_abilities=()):
+    def __init__(self, race_tag, str_mod, dex_mod, con_mod, int_mod, wis_mod, chr_mod, racial_abilities):
         self.nameOfRace = race_tag
         self.strMod = str_mod
         self.dexMod = dex_mod
@@ -18,50 +18,42 @@ class RaceTemplate:
         self.racialAbilities = racial_abilities
 
 
-def race_stat_blocks():
+def set_race():
     race = None
-    race_tag = input("Which one would you like?\n")
+    race_tag = input("Which race would you like your character to be?\n")
     if race_tag == "list races":
         list_races()
         race = None
-        race_stat_blocks()
-    elif race_tag == "dwarf":
-        race = RaceTemplate(race_tag, 0, 0, 2, 0, 0, 0, ("Fey", "Darkvision"))
-    elif race_tag == "elf":
-        race = RaceTemplate(race_tag, 0, 2, 0, 0, 0, 0, ("Fey", "Darkvision"))
-    elif race_tag == "halfling":
-        race = RaceTemplate(race_tag, 0, 2, 0, 0, 0, 0, "")
-    elif race_tag == "human":
-        race = RaceTemplate(race_tag, 0, 2, 0, 0, 0, 0, ("Fey", "Darkvision"))
-    elif race_tag == "half-orc":
-        race = RaceTemplate(race_tag, 2, 0, 1, 0, 0, 0, ("Fey", "Darkvision"))
-    elif race_tag == "dragonborn":
-        race = RaceTemplate(race_tag, 2, 0, 0, 0, 0, 1, ("Fey", "Darkvision"))
-    elif race_tag == "gnome":
-        race = RaceTemplate(race_tag, 0, 0, 0, 2, 0, 0, ("Fey", "Darkvision"))
-    elif race_tag == "half-elf":
-        race = RaceTemplate(race_tag, 0, 0, 0, 0, 0, 2, ("Fey", "Darkvision"))
-        # ##CAN CHOOSE TWO OTHERS TO INCREASE
-        print("You chose a half elf so you can increase two additional stats by one, but I haven't implemented that yet. :(")
-    elif race_tag == "tiefling":
-        race = RaceTemplate(race_tag, 0, 0, 0, 1, 0, 2, ("Fey", "Darkvision"))
+        set_race()
+    elif type(race_tag) == str and race_tag != "list races":
+        race = races[race_tag]
     else:
         print("I did not understand that. PLease try again. or type lsit races to see the list of races.")
         race = None
-        race_stat_blocks()
+        set_race()
 
     if race is not None and race != "list races":
         return race
 
 
+races = {
+    "debug race": RaceTemplate("debug race", 1, 2, 3, 4, 5, 100, "darkvision"),
+    "dwarf": RaceTemplate("dwarf", 0, 0, 2, 0, 0, 0, ["Fey", "Darkvision"]),
+    "elf": RaceTemplate("elf", 0, 2, 0, 0, 0, 0, ("Fey", "Darkvision")),
+    "halfling": RaceTemplate("halfling", 0, 2, 0, 0, 0, 0, ""),
+    "human": RaceTemplate("human", 0, 2, 0, 0, 0, 0, ("Fey", "Darkvision")),
+    "half-orc": RaceTemplate("half-orc", 2, 0, 1, 0, 0, 0, ("Fey", "Darkvision")),
+    "dragonborn": RaceTemplate("dragonborn", 2, 0, 0, 0, 0, 1, ("Fey", "Darkvision")),
+    "gnome": RaceTemplate("gnome", 0, 0, 0, 2, 0, 0, ("Fey", "Darkvision")),
+    "half-elf": RaceTemplate("half-elf", 0, 0, 0, 0, 0, 2, ("Fey", "Darkvision")),
+    # ##CAN CHOOSE TWO OTHERS TO INCREASE
+    "tiefling": RaceTemplate("tiefling", 0, 0, 0, 1, 0, 2, ("Fey", "Darkvision"))
+
+
+
+
+}
 def list_races():
-    print("""The current races are:
-    Dwarf,
-    Elf,
-    Halfling,
-    Human,
-    Dragonborn,
-    Gnome,
-    Half-Elf,
-    Half-Orc,
-    Tiefling""")
+    print("The current races are:")
+    for key, value in races:
+        print(key)
